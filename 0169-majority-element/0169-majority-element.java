cae34c1bc010e74bc8a1c31785a504a522e46ffe
Ganesh-Kudtarkar <68811686+Ganesh-Kdt@users.noreply.github.com>
2024-05-16 20:17:30 +0530

    Time: 22 ms (8.25%), Space: 49 MB (90.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,17 +4,24 @@ class Solution {
         for(int i=0;i<=nums.length-1;i++){
             if(!mp.containsKey(nums[i])){
                 mp.put(nums[i],1);
+                if(mp.get(nums[i])>(int)(nums.length/2)){
+                    return nums[i];
+                }
             }
             else{
                 mp.put(nums[i],mp.get(nums[i])+1);
+                if(mp.get(nums[i])>(int)(nums.length/2)){
+                    return nums[i];
+                }
             }
         }
-        int number=0;
-        for(Map.Entry<Integer,Integer> e : mp.entrySet()){
-            if(e.getValue()>(int)(nums.length/2)){
-                number=e.getKey();
-            }
-        }
-        return number;
+        return 0;
+        // int number=0;
+        // for(Map.Entry<Integer,Integer> e : mp.entrySet()){
+        //     if(e.getValue()>(int)(nums.length/2)){
+        //         number=e.getKey();
+        //     }
+        // }
+        // return number;
     }
 }
