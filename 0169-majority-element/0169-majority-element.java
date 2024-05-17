9f45e88c95e2402a890aba44d9314b5c5cf68a23
Ganesh-Kudtarkar <68811686+Ganesh-Kdt@users.noreply.github.com>
2024-05-17 12:09:52 +0530

    Time: 2 ms (78.37%), Space: 52.9 MB (53.00%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,27 +1,39 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mp = new HashMap<Integer,Integer>();
+       /*
+       Using HashMap for storing frequency of each element
+       HashMap<Integer,Integer> mp = new HashMap<Integer,Integer>();
         for(int i=0;i<=nums.length-1;i++){
             if(!mp.containsKey(nums[i])){
                 mp.put(nums[i],1);
-                if(mp.get(nums[i])>(int)(nums.length/2)){
-                    return nums[i];
-                }
             }
             else{
                 mp.put(nums[i],mp.get(nums[i])+1);
-                if(mp.get(nums[i])>(int)(nums.length/2)){
-                    return nums[i];
-                }
             }
         }
-        return 0;
-        // int number=0;
-        // for(Map.Entry<Integer,Integer> e : mp.entrySet()){
-        //     if(e.getValue()>(int)(nums.length/2)){
-        //         number=e.getKey();
-        //     }
-        // }
-        // return number;
+        int number=0;
+        for(Map.Entry<Integer,Integer> e : mp.entrySet()){
+            if(e.getValue()>(int)(nums.length/2)){
+                number=e.getKey();
+            }
+        }
+        return number;
+        */
+        //Moore Voting Algorithm
+        int ME=nums[0];
+        int count=1;
+        for(int i=1;i<=nums.length-1;i++){
+            if(nums[i]==ME){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                ME=nums[i];
+                count=1;
+            }
+        }
+        return ME;
     }
 }
