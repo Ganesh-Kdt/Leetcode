d8e978dd7dffbc47497665ab9ab5298ad6ce7d0b
Ganesh-Kudtarkar <68811686+Ganesh-Kdt@users.noreply.github.com>
2024-07-03 06:52:34 +0530

    Time: 4 ms (18.03%), Space: 45.7 MB (8.40%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -3,14 +3,9 @@ class Solution {
         int index=-1;
         for(int i=num.length()-1;i>=0;i--){
             if(Integer.parseInt(String.valueOf(num.charAt(i)))%2!=0){
-                index=i;
-                break;
+                return num.substring(0,i+1); 
             }
         }
-        String s="";
-        for(int i=0;i<=index;i++){
-            s=s+num.charAt(i);
-        }
-        return s;
+        return "";
     }
 }
